afd2610fbfbe4426f539732eaf4db1ead9c0bd51
stefanpython <stefanhtml@yahoo.com>
2025-03-19 14:10:22 +0200

    Update: add register function

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -2,13 +2,23 @@ export const login = async (username: string, password: string) => {
   const response = await fetch("http://127.0.0.1:8000/token", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams({ username, password }),
+    body: new URLSearchParams({ username, password, grant_type: "password" }),
   });
 
   if (!response.ok) throw new Error("Invalid credentials");
 
   const data = await response.json();
   localStorage.setItem("token", data.access_token);
+};
+
+export const register = async (username: string, password: string) => {
+  const response = await fetch("http://127.0.0.1:8000/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+
+  if (!response.ok) throw new Error("Registration failed");
 };
 
 export const logout = () => {
