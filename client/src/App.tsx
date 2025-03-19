eee4c8196ccef7df4c8f2bb9600fce55aba8d338
stefanpython <stefanhtml@yahoo.com>
2025-03-19 14:13:34 +0200

    Update: add protected route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,28 @@
-import "./App.css";
-// import Users from "./components/Users";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import TodoApp from "./components/TodoApp";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { isAuthenticated } from "./auth";
 
 function App() {
   return (
-    <>
-      <div className="flex justify-center">
-        <TodoApp />
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthenticated() ? <TodoApp /> : <Navigate to="/login" replace />
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
